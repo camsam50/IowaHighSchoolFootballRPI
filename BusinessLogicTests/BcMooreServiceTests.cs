e42003051cc9ffbb673b69e2aed5f6c2bcaf747d
Jay Campbell <43826300+camsam50@users.noreply.github.com>
2021-01-27 11:27:55 -0600

    wire up fake tests

---
+++ BusinessLogicTests/BcMooreServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Services.Tests
+{
+    [TestClass()]
+    public class BcMooreServiceTests
+    {
+        [TestMethod()]
+        public void DoWorkTest()
+        {
+            throw new NotImplementedException();
+        }
+        
+        [TestMethod()]
+        public void DoSomeOtherWorkTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/BcMooreService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class BcMooreService
+    {
+
+
+        public bool DoWork()
+        { 
+            return false;
+        }
+
+        public bool DoSomeOtherWork()
+        {
+            return false;
+        }
+
+    }
+}
