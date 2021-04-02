8dc4ac252944f95c5ba4faa4ccb413d93a414bda
Evan Hennis <nurl_50@hotmail.com>
2021-04-02 11:40:50 -0500

    Added RPI calculations

---
+++ BusinessLogicTests/EvansBullshit/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTests.EvansBullshit
+{
+    public class Game
+    {
+        public int Week { get; set; }
+        public Team Winner { get; set; }
+        public Team Loser { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}
---
+++ BusinessLogicTests/EvansBullshit/Season.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTests.EvansBullshit
+{
+    public class Season
+    {
+        public List<Game> Games { get; set; }
+    }
+}
